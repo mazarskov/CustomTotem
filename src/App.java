0f8ac97f46b254268589b833c6702079895f86f0
mazarskov <maxazarskov@gmail.com>
2024-03-19 19:28:14 +0200

    Improved GUI, Added file selection logic, code still messy

--- src/App.java
+++ src/App.java
@@ -13,62 +13,105 @@ import java.nio.file.Paths;
 import java.awt.Font.*;
 import javax.swing.*;
 public class App {
+    public static String absolutePath = "";
     public static void main(String[] args) {
-       
+        //FlowLayout experimentLayout = new FlowLayout();
+        GridLayout layout = new GridLayout();
+        //experimentLayout.setAlignment(FlowLayout.LEADING);
         boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+        //JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 20));
 
-        JFrame f = new JFrame();//creating instance of JFrame  
+        JFrame f = new JFrame("CustomTotem ALPHA");//creating instance of JFrame  
         JLabel label = new JLabel("hello!");
-        
-        label.setBounds(15,15,300,100);
+        label.setFont(new Font("Arial", Font.PLAIN, 40));
+        label.setBounds(40,200,600,100);
         JButton b = new JButton("Create pack!");//creating instance of JButton 
-        b.setBounds(125,100,150, 40);//x axis, y axis, width, height  
-        b.addActionListener(new ActionListener() {
+        JButton c = new JButton("Choose image.");//creating instance of JButton 
+        JButton d = new JButton("Print path to image.");//creating instance of JButton 
+        //JPanel panel = new JPanel(new GridBagLayout());
+        //GridBagConstraints gbc = new GridBagConstraints();
+        //gbc.weighty = 1;
+        //gbc.weightx = 1;
+        //gbc.fill = GridBagConstraints.HORIZONTAL;
+        //gbc.gridwidth = GridBagConstraints.REMAINDER;
+        //gbc.anchor = GridBagConstraints.SOUTH;
+        b.setBounds(125,300,150, 30);//x axis, y axis, width, height  
+        c.setBounds(300,300,150, 30);//x axis, y axis, width, height  
+        d.setBounds(475,300,150, 30);
+        d.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (isWindows) {
-                    JFileChooser fileChooser = new JFileChooser();
-                    fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
-                    FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files","png");
-                    fileChooser.setFileFilter(filter);
-                    int result = fileChooser.showOpenDialog(f);
-                    if (result == JFileChooser.APPROVE_OPTION) {
-                        File selectedFile = fileChooser.getSelectedFile();
-                        String extension = getFileExtension(selectedFile);
-                        if (extension != null && extension.equals("png")) {
-                            // Process the selected PNG file
-                            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-                            String path = selectedFile.getAbsolutePath();
-                            File selectedFile2 = new File(path);
-                            String destinationFolder = "C:\\Users\\Max\\AppData\\Roaming\\.minecraft\\resourcepacks";
-                            copyImage(selectedFile2, destinationFolder);
-                        } else {
-                            // Show a message to the user indicating only PNG files are allowed
-                            JOptionPane.showMessageDialog(f, "Only PNG files are allowed.", "Invalid File", JOptionPane.ERROR_MESSAGE);
-                        }
+                System.out.println(absolutePath);
+            }
+        });
+        c.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files","png");
+                fileChooser.setFileFilter(filter);
+                int result = fileChooser.showOpenDialog(f);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    String extension = getFileExtension(selectedFile);
+                    if (extension != null && extension.equals("png")) {
+                        // Process the selected PNG file
+                        System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+                        label.setFont(new Font("Arial", Font.PLAIN, 20));
+                        label.setText("Selected file: " + selectedFile.getAbsolutePath());
+                        absolutePath = selectedFile.getAbsolutePath();
                     } else {
-                        macFileCreator();
+                        // Show a message to the user indicating only PNG files are allowed
+                        JOptionPane.showMessageDialog(f, "Only PNG files are allowed.", "Invalid File", JOptionPane.ERROR_MESSAGE);
                     }
                 }
             }
         });
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (isWindows) {
+                            String path = absolutePath;
+                            if (absolutePath.equals("")) {
+                                JOptionPane.showMessageDialog(f, "No Image file selected", "Invalid File", JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                File selectedFile2 = new File(path);
+                                String destinationFolder = windowsFileCreator();
+                                copyImage(selectedFile2, destinationFolder);
+                                JOptionPane.showMessageDialog(f, "Resource Pack with a custom totem created!", "Success!", JOptionPane.INFORMATION_MESSAGE);
+                                System.exit(0);
+                            }
+
+                    } else {
+                        macFileCreator();
+                    }
+                }
+            });
         
         
+        //panel.add(label); 
+        //panel.add(c);
+        //panel.add(b);//adding button in JFrame   
+        //f.add(c);
+        f.add(label);
+        f.add(c);
+        f.add(b);
+        f.add(d);
         
         
-        
-        
-        
-        f.add(b);//adding button in JFrame   
-        f.add(label); 
-        f.setSize(400,200);//400 width and 500 height  
+        //f.getContentPane().add(panel);
+        f.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        f.validate();
+        f.repaint();
+        f.setSize(800,400);//400 width and 500 height  
         f.setLayout(null);//using no layout managers  
         f.setVisible(true);//making the frame visible  
         f.setResizable(false);
     }
 
 
-    public static void windowsFileCreator() {
+    public static String windowsFileCreator() {
         try {
             String resourcePath = System.getProperty("user.home") + "\\AppData\\Roaming\\.minecraft\\resourcepacks";
             new File(resourcePath + "\\My_pack\\assets\\minecraft\\textures\\item").mkdirs();
@@ -80,8 +123,10 @@ public class App {
             fileWriter.write(content);
             fileWriter.close();
             System.out.println("Mcmeta file filled with info");
+            return resourcePath + "\\My_pack\\assets\\minecraft\\textures\\item";
         } catch (IOException e) {
             e.printStackTrace();
+            return "error";
         }
     }
 
@@ -117,7 +162,7 @@ public class App {
             Path destinationFolderPathObj = Paths.get(destinationFolderPath);
             
             // Assign a static filename for the copied file
-            String fileName = "static_image.png";
+            String fileName = "totem_of_undying.png";
 
             // Create a Path object for the destination file
             Path destinationFilePath = destinationFolderPathObj.resolve(fileName);
